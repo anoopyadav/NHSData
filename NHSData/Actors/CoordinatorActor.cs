320ded80248a3dea6fce8a3488c6faca8a5770d0
Anoop Yadav <anzy.neatfreak@gmail.com>
2017-09-09 16:51:06 +0100

    Added project dependencies - akka.net, NLog, configurations

---
+++ NHSData/Actors/CoordinatorActor.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Actor;
+using Akka.Event;
+
+namespace NHSData.Actors
+{
+    public class CoordinatorActor : ReceiveActor
+    {
+        private readonly ILoggingAdapter _logger;
+
+        public CoordinatorActor()
+        {
+            _logger = Context.GetLogger();
+            _logger.Info("Coordinator Initailized");
+        }
+    }
+}
---
+++ NHSData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Event;
+using NHSData.Actors;
+
+namespace NHSData
+{
+    public class Program
+    {
+        private static ActorSystem _nhsDataAnalysisActorSystem;
+        private static ILoggingAdapter _logger;
+        static void Main(string[] args)
+        {
+            _nhsDataAnalysisActorSystem = ActorSystem.Create("NhsDataAnalysis");
+            _logger = _nhsDataAnalysisActorSystem.Log;
+
+            _logger.Info("NhsDataAnalysis ActorSystem initialised");
+
+            _nhsDataAnalysisActorSystem.ActorOf<CoordinatorActor>("Coordinator");
+
+            _nhsDataAnalysisActorSystem.Terminate().Wait();
+        }
+    }
+}
